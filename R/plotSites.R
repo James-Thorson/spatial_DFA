18c4b4909548f451e9db41cf4234b7457ae46e81
James-Thorson <JamesT.esq@gmail.com>
2015-03-23 11:36:35 -0700

    Small change to plotting tool

--- R/plotSites.R
+++ R/plotSites.R
@@ -4,9 +4,13 @@ function( mapObj, x, y, col, cex=2, ... ){
   require(maptools)
 
   # Plot map
-  plot(1, type="n", xlim=attr(mapObj,"bbox")['x',], ylim=attr(mapObj,"bbox")['y',], ...)
-  if( length(mapObj)>0 ) plot(mapObj, add=TRUE)
-
+  if( is.list(mapObj) ){
+    plot(mapObj, type="l", xlim=mapObj[["bbox"]]['x',], ylim=mapObj[["bbox"]]['y',], ...)
+  }else{
+    plot(1, type="n", xlim=attr(mapObj,"bbox")['x',], ylim=attr(mapObj,"bbox")['y',], ...)
+    if( length(mapObj)>0 ) suppressWarnings( plot(mapObj, add=TRUE) )
+  }
+  
   #plot site locations
   points(x=x, y=y, pch=20, cex=cex, col=col)
 }
