7fa82900e701584c85d9ba845c52556b8866ed27
James-Thorson <JamesT.esq@gmail.com>
2015-06-09 16:46:34 -0700

    Added new TMB versions

--- R/plotSites.R
+++ R/plotSites.R
@@ -4,11 +4,11 @@ function( mapObj, x, y, col, cex=2, ... ){
   require(maptools)
 
   # Plot map
-  if( is.list(mapObj) ){
+  if( is.list(mapObj) & length(mapObj)>0 ){
     plot(mapObj, type="l", xlim=mapObj[["bbox"]]['x',], ylim=mapObj[["bbox"]]['y',], ...)
   }else{
     plot(1, type="n", xlim=attr(mapObj,"bbox")['x',], ylim=attr(mapObj,"bbox")['y',], ...)
-    if( length(mapObj)>0 ) suppressWarnings( plot(mapObj, add=TRUE) )
+    if( length(mapObj)>1 ) suppressWarnings( plot(mapObj, add=TRUE) )
   }
   
   #plot site locations
